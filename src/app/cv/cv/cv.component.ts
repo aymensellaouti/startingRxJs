f0ba64f287a3b0808ec327edaf09ebd22b178b36
aymen2782 <aymen.sellaouti@gmail.com>
2023-05-24 10:58:16 +0100

    fix cvComponent issue

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
-import { Cv } from '../model/cv';
-import { LoggerService } from '../../services/logger.service';
-import { ToastrService } from 'ngx-toastr';
-import { CvService } from '../services/cv.service';
-import { EMPTY, Observable, catchError, of } from 'rxjs';
+import { Component } from "@angular/core";
+import { Cv } from "../model/cv";
+import { LoggerService } from "../../services/logger.service";
+import { ToastrService } from "ngx-toastr";
+import { CvService } from "../services/cv.service";
+import { EMPTY, Observable, catchError, of } from "rxjs";
 @Component({
-  selector: 'app-cv',
-  templateUrl: './cv.component.html',
-  styleUrls: ['./cv.component.css'],
+  selector: "app-cv",
+  templateUrl: "./cv.component.html",
+  styleUrls: ["./cv.component.css"],
 })
 export class CvComponent {
-  cvs$: Observable<Cv[]>;
+  cvs: Cv[] = [];
   nbClickItem = 0;
   /*   selectedCv: Cv | null = null; */
   date = new Date();
@@ -20,29 +20,19 @@ export class CvComponent {
     private toastr: ToastrService,
     private cvService: CvService
   ) {
-    this.cvs$ = this.cvService.getCvs().pipe(
-      catchError(
-        (e) => {
-          this.toastr.error(`
+    this.cvService.getCvs().subscribe({
+      next: (cvs) => {
+        this.cvs = cvs;
+      },
+      error: () => {
+        this.cvs = this.cvService.getFakeCvs();
+        this.toastr.error(`
           Attention!! Les données sont fictives, problème avec le serveur.
-          Veuillez contacter l'admin.`)
-          return of(this.cvService.getFakeCvs());
-        }
-      )
-    );
-    /* this.cvService.getCvs().subscribe(
-      {
-        next: (cvs) => {this.cvs = cvs;},
-        error: () => {
-          this.cvs = this.cvService.getFakeCvs();
-          this.toastr.error(`
-          Attention!! Les données sont fictives, problème avec le serveur.
-          Veuillez contacter l'admin.`)
-        }
-      }
-    ); */
-    this.logger.logger('je suis le cvComponent');
-    this.toastr.info('Bienvenu dans notre CvTech');
+          Veuillez contacter l'admin.`);
+      },
+    });
+    this.logger.logger("je suis le cvComponent");
+    this.toastr.info("Bienvenu dans notre CvTech");
     this.cvService.selectCv$.subscribe(() => this.nbClickItem++);
   }
 }
